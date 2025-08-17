98ecb5eee3f7cde7990c440f3669798608d42bc0
Hamid Ebadi <hamide@ziprecruiter.com>
2025-08-16 18:49:57 -0700

    fix: fix reference

--- app/src/main/java/com/app1/personalpocketdictionary/ui/devnotes/DevNotesScreen.kt
+++ app/src/main/java/com/app1/personalpocketdictionary/ui/devnotes/DevNotesScreen.kt
@@ -1,15 +1,25 @@
 package com.app1.personalpocketdictionary.ui.devnotes
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -29,7 +39,7 @@ fun DevNotesScreen(
 ) {
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(stringResource(R.string.title_dev_notes)) }) // Assuming a string resource for title
+            TopAppBar(title = { Text(stringResource(R.string.dev_notes)) }) // Assuming a string resource for title
         }
     ) { paddingValues ->
         Column(
